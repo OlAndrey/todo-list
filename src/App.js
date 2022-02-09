dab2acbe4fde076652c6ecda39305e252285b8c8
OlAndrey <oleynik.andrey01@gmail.com>
2022-02-09 18:45:12 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import TodoItem from './Components/TodoItem';
+import TodoAdd from './Components/TodoAdd';
+import todosDate from './todosDate.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App(){
+  let [state, setState] = useState(todosDate)
+
+
+  function handlerChange(id){
+    setState(
+      state.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    )
+    }
+
+    function addTodo(title){
+      setState(
+        state.concat({
+          "userId": 1,
+          "id": state.length+1,
+          "title": title,
+          "completed": false
+        })
+      )
+    }
+
+    function removeTodo(id){
+      setState(
+        state.filter(todo => todo.id !== id)
+      )
+    }
+
+
+    let todos = state.map((item) => <TodoItem key={item.id} item={item} onChange={handlerChange} onClick={removeTodo} />);
+    
+    return (
+      <div className="App">
+        <div className="wrapper">
+          <TodoAdd onChange={addTodo} />
+          <ul className="todo-list">
+            {todos}
+          </ul>
+        </div>
+      </div>
+    );
+  
 }
 
 export default App;
